abf022190330c6ea6355adf8de02eac10d2c8d0b
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2023-05-18 00:16:49 +0600

    cоздание шаблона pygame

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+WIDTH, HEIGHT = 700, 800
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('minesweeper')
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
